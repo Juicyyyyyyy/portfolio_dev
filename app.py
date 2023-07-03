e33563eda0123b4f5658cc602ad91006d6a01eb1
Juicyyyyyyy <corentin.dupaigne@gmail.com>
2023-07-03 22:24:10 +0200

    add psycopg2-binary

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from app import app, db
-import os
-
-with app.app_context():
-	db.create_all()
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+	app.run(debug=False)
