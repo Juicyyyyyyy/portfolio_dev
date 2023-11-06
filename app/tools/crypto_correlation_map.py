174e80692ccd1bd46be4e391449a22c53f865b21
Juicyyyyyyy <declan.lowee@gmail.com>
2023-11-06 19:43:06 +0100

    Implemented plotly

--- app/tools/crypto_correlation_map.py
+++ app/tools/crypto_correlation_map.py
@@ -1,40 +1,70 @@
-import openai
-from multiprocessing import Pool
 import os
 from dotenv import load_dotenv
+import pandas as pd
 from flask import Flask, request, jsonify, Blueprint, render_template
 from .trading_functions import HistoricalDataFetcher
+from datetime import datetime
+from app.models.cryptos import CryptoPrice, Cryptocurrency
+from app import db
+from sqlalchemy import and_
+import plotly.graph_objs as go
 
 data_fetcher = HistoricalDataFetcher()
 
 crypto_correlation_route = Blueprint('crypto_correlation_map', __name__)
 
 @crypto_correlation_route.route('/crypto_correlation_map', methods=['GET', 'POST'])
-def create_correlation_map():
+def create_correlation_matrix():
     if request.method == 'POST':
         # Get the form values submitted by the user
         selected_symbols = request.form.getlist('symbols')
-        timeframe = request.form['timeframe']
-        since_timestamp = int(request.form['since_timestamp'])
-        till_timestamp = int(request.form['till_timestamp'])
 
-        # Fetch historical data for the selected symbols
+        # Convert selected dates to timestamps
+        since_date = datetime.strptime(request.form['since_date'], '%Y-%m-%d')
+        till_date = datetime.strptime(request.form['till_date'], '%Y-%m-%d')
+
+        # Fetch historical data from the database
         historical_data = {}
         for symbol in selected_symbols:
-            df = data_fetcher.fetch_historical_data(symbol, timeframe, since_timestamp, till_timestamp)
+            # Query the CryptoPrice table for the selected symbol and date range
+            data = db.session.query(CryptoPrice).join(CryptoPrice.crypto).filter(
+                and_(Cryptocurrency.symbol == symbol, CryptoPrice.date >= since_date, CryptoPrice.date <= till_date)
+            ).all()
+
+            # Convert the data to a DataFrame
+            df = pd.DataFrame([(entry.date, entry.open, entry.high, entry.low, entry.close, entry.volume) for entry in data],
+                              columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+            # Calculate returns
             returns = data_fetcher.calculate_returns(df)
             historical_data[symbol] = returns
 
         # Calculate correlations
         correlation_data = {}
         for symbol, returns in historical_data.items():
-            correlation_matrix = data_fetcher.calculate_correlations(returns)
+            correlation_matrix = data_fetcher.calculate_correlations([returns])
             correlation_data[symbol] = correlation_matrix
 
-        # Pass the correlation data and form parameters to the template
-        return render_template('crypto_correlation_map.html', correlation_data=correlation_data,
-                               selected_symbols=selected_symbols, timeframe=timeframe,
-                               since_timestamp=since_timestamp, till_timestamp=till_timestamp)
+            # Create a correlation matrix plot
+            plot_data = []
+            for symbol, corr_matrix in correlation_data.items():
+                fig = go.Figure(data=go.Heatmap(
+                    z=corr_matrix.values,
+                    x=corr_matrix.columns,
+                    y=corr_matrix.index,
+                    colorscale='Viridis',
+                ))
+                fig.update_layout(
+                    title=f'Correlation Matrix for {symbol}',
+                    xaxis_title='Cryptocurrencies',
+                    yaxis_title='Cryptocurrencies',
+                )
+                plot_data.append(fig.to_html(full_html=False, include_plotlyjs='cdn'))
+
+            return render_template('crypto_correlation_map.html', plot_data=plot_data,
+                                   selected_symbols=selected_symbols,
+                                   since_date=since_date, till_date=till_date)
     else:
-        # Render the initial form with default parameters
-        return render_template('crypto_correlation_map.html')
+        cryptocurrencies = db.session.query(Cryptocurrency).all()
+        return render_template('crypto_correlation_map.html', cryptocurrencies=cryptocurrencies)
--- populate_db/cryptos_data.py
+++ populate_db/cryptos_data.py
@@ -11,8 +11,8 @@ def fetch_and_store_historical_crypto_data():
     binance = ccxt.binance()
 
     # Define the list of cryptocurrency symbols you want to fetch
-    symbols = ["BTC/USDT", "ETH/USDT", "XRP/USDT", "BNB/USDT", "USDC/USDT", "SOL/USDT", "ADA/USDT", "DOGE/USDT", "TRX/USDT", "TON/USDT", "LINK/USDT", "MATIC/USDT", "DOT/USDT", "WBTC/USDT", "DAI/USDT", "LTC/USDT", "SHIB/USDT", "BCH/USDT", "AVAX/USDT", "LEO/USDT", "XLM/USDT", "TUSD/USDT", "OKB/USDT", "ATOM/USDT", "XMR/USDT", "UNI/USDT", "ETC/USDT", "FIL/USDT", "BUSD/USDT", "HBAR/USDT", "ICP/USDT", "LDO/USDT", "CRO/USDT", "APT/USDT", "NEAR/USDT", "VET/USDT", "ARB/USDT", "OP/USDT", "INJ/USDT", "AAVE/USDT", "MKR/USDT", "MNT/USDT", "QNT/USDT", "EGLD/USDT", "GRT/USDT", "IMX/USDT", "RUNE/USDT", "ALGO/USDT", "NEO/USDT", "BSV/USDT", "STX/USDT", "RNDR/USDT", "AXS/USDT", "THETA/USDT", "SAND/USDT", "XTZ/USDT", "MANA/USDT", "EOS/USDT", "FTM/USDT", "XDC/USDT", "USDD/USDT", "SNX/USDT", "FDUSD/USDT", "KAVA/USDT", "BGB/USDT", "CHZ/USDT", "FLOW/USDT", "MINA/USDT", "XEC/USDT", "CFX/USDT", "TWT/USDT", "CAKE/USDT", "APE/USDT", "GALA/USDT", "KCS/USDT", "IOTA/USDT", "CRV/USDT", "RPL/USDT", "XAUt/USDT", "PEPE/USDT", "FXS/USDT", "SUI/USDT", "PAXG/USDT", "ZEC/USDT", "KLAY/USDT", "USDP/USDT", "ROSE/USDT", "ETHDYDX/USDT", "AR/USDT", "BTT/USDT", "GMX/USDT", "CSPR/USDT", "WOO/USDT", "COMP/USDT", "HT/USDT", "RUNE/USDT"]
-
+    #symbols = ["BTC/USDT", "ETH/USDT", "XRP/USDT", "BNB/USDT", "USDC/USDT", "SOL/USDT", "ADA/USDT", "DOGE/USDT", "TRX/USDT", "TON/USDT", "LINK/USDT", "MATIC/USDT", "DOT/USDT", "WBTC/USDT", "DAI/USDT", "LTC/USDT", "SHIB/USDT", "BCH/USDT", "AVAX/USDT", "LEO/USDT", "XLM/USDT", "TUSD/USDT", "OKB/USDT", "ATOM/USDT", "XMR/USDT", "UNI/USDT", "ETC/USDT", "FIL/USDT", "BUSD/USDT", "HBAR/USDT", "ICP/USDT", "LDO/USDT", "CRO/USDT", "APT/USDT", "NEAR/USDT", "VET/USDT", "ARB/USDT", "OP/USDT", "INJ/USDT", "AAVE/USDT", "MKR/USDT", "MNT/USDT", "QNT/USDT", "EGLD/USDT", "GRT/USDT", "IMX/USDT", "RUNE/USDT", "ALGO/USDT", "NEO/USDT", "BSV/USDT", "STX/USDT", "RNDR/USDT", "AXS/USDT", "THETA/USDT", "SAND/USDT", "XTZ/USDT", "MANA/USDT", "EOS/USDT", "FTM/USDT", "XDC/USDT", "USDD/USDT", "SNX/USDT", "FDUSD/USDT", "KAVA/USDT", "BGB/USDT", "CHZ/USDT", "FLOW/USDT", "MINA/USDT", "XEC/USDT", "CFX/USDT", "TWT/USDT", "CAKE/USDT", "APE/USDT", "GALA/USDT", "KCS/USDT", "IOTA/USDT", "CRV/USDT", "RPL/USDT", "XAUt/USDT", "PEPE/USDT", "FXS/USDT", "SUI/USDT", "PAXG/USDT", "ZEC/USDT", "KLAY/USDT", "USDP/USDT", "ROSE/USDT", "ETHDYDX/USDT", "AR/USDT", "BTT/USDT", "GMX/USDT", "CSPR/USDT", "WOO/USDT", "COMP/USDT", "HT/USDT", "RUNE/USDT"]
+    symbols = ["BTC/USDT", "ETH/USDT", "XRP/USDT"]
     # Define the start date (5 years ago from today)
     start_date = datetime.now() - timedelta(days=5*365)
 
@@ -24,7 +24,7 @@ def fetch_and_store_historical_crypto_data():
                 # Fetch OHLCV data for the current day and symbol
                 since = int((current_date - timedelta(days=1)).timestamp()) * 1000
                 till = int(current_date.timestamp()) * 1000
-                ohlcv = binance.fetch_ohlcv(symbol, '1d', since, till)
+                ohlcv = binance.fetch_ohlcv(symbol, '1w', since, till)
 
                 # Check if the cryptocurrency exists in the database
                 crypto = Cryptocurrency.query.filter_by(symbol=symbol).first()
